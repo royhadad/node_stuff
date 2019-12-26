24e7f53d8ae6180c21832dd1b23b16cb72c81505
Roy Hadad <royhadad98@gmail.com>
2019-12-26 19:28:53 +0200

    adding the getuser by id and create user queries

--- michael_project/index.js
+++ michael_project/index.js
@@ -1,16 +1,119 @@
 const express = require('express');
 const Joi = require('joi');
+const users = require('./users.js');
+const ResponseObj = require('./ResponseObj');
 const app = express();
+app.use(express.json());
 PORT_NUMBER = 3000;
+const minimumUsernameLength = 3;
+const maximumUsernameLength = 20;
 
+const minimumPasswordLength = 6;
+const maximumPasswordLength = 20;
 
-app.get('/aba', (req, res) =>{
-    res.send("responding!");
-})
+const minimumFullNameLength = 5;
+const maximumFullNameLength = 30;
 
-app.post('/aba', (req, res) =>{
-
-    res.send("added!");
+app.get('/users/getUser/:id', (req, res) =>
+{
+    let responseObj = new ResponseObj();
+    users.getUserById(req.params.id)
+        .then((result) =>
+        {
+            if (result)
+                responseObj.data = result;
+            else
+                responseObj.error = new Error("user not found!");
+            res.send(JSON.stringify(responseObj));
+        })
+        .catch((error) =>
+        {
+            responseObj.error = error;
+            res.status(400).send(JSON.stringify(responseObj));
+        });
 });
 
-app.listen(PORT_NUMBER, ()=>console.log(`listening on port ${PORT_NUMBER}...`));
+app.get('/favicon.ico', (req, res) =>
+{
+    res.status(204);
+});
+
+app.post('/users/createUser', (req, res) =>
+{
+    let responseObj = new ResponseObj();
+    let userObj = req.body;
+    const schema = {
+        username: Joi.string().min(minimumUsernameLength).max(maximumUsernameLength).regex(/\w*/).required(),
+        password: Joi.string().min(minimumPasswordLength).max(maximumPasswordLength).regex(/\S*/).required(),
+        full_name: Joi.string().min(minimumFullNameLength).max(maximumFullNameLength).regex(/\D*/).required(),
+        about: Joi.string().required()
+    };
+    let validationResult = Joi.validate(userObj, schema);
+    if (validationResult.error)
+    {
+        responseObj.error = validationResult.error.details.reduce((sum, current) => sum + current + "\n", "");
+        res.status(500).send(responseObj);
+    }
+    else
+    {
+        users.createUser(userObj)
+            .then((result) =>
+            {
+                if (result)
+                    responseObj.data = result;
+                else
+                    responseObj.error = new Error("couldnt create new user!");
+                res.status(500).send(JSON.stringify(responseObj));
+            })
+            .catch((error) =>
+            {
+                responseObj.error = error;
+                res.status(400).send(JSON.stringify(responseObj));
+            });
+    }
+});
+
+app.post('users/login', (req, res) =>
+{
+    let responseObj = new ResponseObj();
+    let loginDetailsObj = req.body;
+    const schema = {
+        username: Joi.string().min(minimumUsernameLength).max(maximumUsernameLength).regex(/\w*/).required(),
+        password: Joi.string().min(minimumPasswordLength).max(maximumPasswordLength).regex(/\S*/).required(),
+    };
+    let validationResult = Joi.validate(loginDetailsObj, schema);
+    if (validationResult.error)
+    {
+        responseObj.error = validationResult.error.details.reduce((sum, current) => sum + current + "\n", "");
+        res.status(400).send(responseObj);
+    }
+    else
+    {
+        //get authorization key!
+        res.send("authorization_key_place_holder");
+    }
+});
+
+app.put('/users/editUser', (req, res) =>
+{
+    let responseObj = new ResponseObj();
+    //check for auth
+
+    
+
+
+
+
+});
+
+app.put('/users/changePassword', (req, res) =>
+{
+    let responseObj = new ResponseObj();
+    //check for auth
+
+
+
+
+});
+
+app.listen(PORT_NUMBER, () => console.log(`listening on port ${PORT_NUMBER}...`));
--- michael_project/users.js
+++ michael_project/users.js
@@ -1,8 +1,48 @@
-function addUser(user)
-{
+const mysql = require("mysql");
 
-
+module.exports = {
+    getUserById: (id) =>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            const connection = getNewConnection();
+            connection.connect();
+            connection.query(`SELECT * FROM users WHERE id=${id}`, function (error, results, fields)
+            {
+                connection.end();
+                if (error) reject(error);
+                resolve(results[0]);
+            });
+        });
+    },
+    createUser: (user) =>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            const connection = getNewConnection();
+            connection.connect();
+            connection.query(`INSERT INTO users (username, password_hash, full_name, about) VALUES("${user.username}", "${user.password}", "${user.full_name}", "${user.about}")`, function (error, results, fields)
+            {
+                connection.end();
+                if (error) reject(error);
+                resolve(results);
+            });
+        });
+    }
 }
+
+function getNewConnection()
+{
+    return mysql.createConnection(
+        {
+            host: 'royhadadusers.ctblchui610j.us-east-2.rds.amazonaws.com',
+            user: 'admin',
+            password: '12345678',
+            database: 'usersDB'
+        }
+    );
+}
+
 function changePassword(id, newPassword)
 {
 
